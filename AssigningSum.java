6348f43994b266f286f9efb9afa7754beb5491a5
Darron C. <183058256+Darrond13@users.noreply.github.com>
2025-05-26 18:44:05 -0500

    CourseWork
    University HW

---
+++ AssigningSum.java
@@ -0,0 +1,19 @@
+public class AssigningSum {
+    public static void main(String[] args) {
+        int numCoins;
+        int numDimes;
+        int numQuarters;
+
+        numQuarters = 25;
+        numDimes = 10;
+
+        numCoins = numQuarters + numDimes;
+        
+        System.out.print("There are ");
+        System.out.print(numCoins);
+        System.out.println(" coins");
+
+
+    }
+
+}
