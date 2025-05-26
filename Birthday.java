d985c70d0cdf1024c29ded78ca685c12743c8385
Darron C. <183058256+Darrond13@users.noreply.github.com>
2025-05-26 18:20:08 -0500

    CourseWork
    University HW

---
+++ Birthday.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Birthday {
+    public static void main(String [] args) {
+        Scanner scnr = new Scanner(System.in);
+        int birthMonth;
+        int birthYear;
+
+        birthMonth = scnr.nextInt();
+        birthYear = scnr.nextInt();
+        System.out.println(birthMonth + "/" + birthYear);
+        
+    }
+
+
+}
